51dbe80a0323583da48e90ce49cc661256d65445
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-21 22:19:59 +0530

    Largest of two no.

---
+++ Largest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Largest {
+    public static void main(String args[]){
+        System.out.println("enter first no. = ");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("enter second no. = ");
+        int b=sc.nextInt();
+
+        if(a>b){
+            System.out.println("first no. is greater");
+
+        }
+         else{
+            System.out.println("Second no. is Greater");
+        }
+
+    }
+    
+}
