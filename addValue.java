9417a02b19ca4b95a7c0add052f7cc240a763e2f
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-26 23:35:08 +0530

    add new element in array

---
+++ addValue.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class addValue {
+    public static void main(String args[]){
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter no. of element in array");
+int n=sc.nextInt();
+int a[]=new int[n];
+int b[]=new int[n+1];
+System.out.println("enter values");
+for(int i=0;i<n;i++){
+    a[i]=sc.nextInt();
+}
+System.out.println("enter index of new value inserted");
+int m=sc.nextInt();
+System.out.println("enter new value inserted");
+int p=sc.nextInt();
+
+for(int i=0;i<n+1;i++){
+    if(i<m){
+        b[i]=a[i];
+    }
+    else if(i==m){
+        b[i]=p;
+        
+    }
+    else b[i]=a[i-1];
+
+}
+System.out.println("Element are: ");
+for(int i=0;i<n+1;i++){
+    System.out.println(b[i]);
+}
+
+    }
+}
