050366d7e22b8b0247fd1178707868718dd3abb6
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-26 23:33:59 +0530

    check even no.

---
+++ even.java
@@ -0,0 +1,16 @@
+//even no. upto 50
+public class even 
+{  
+public static void main(String args[])   
+{  
+int number=50;  
+System.out.print("List of even numbers from 1 to "+number+": ");  
+for (int i=1; i<=number; i++)   
+{  
+if (i%2==0)   
+{  
+System.out.println(i + " ");  
+}  
+}  
+}  
+}  
