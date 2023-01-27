550317651741eb1ee9e22ad9ac0a611637b73e47
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-27 23:42:03 +0530

    looping in array

---
+++ loopArray.java
@@ -0,0 +1,9 @@
+public class loopArray {
+    public static void main(String args[]){
+        int[] age={5,10,18,};
+        for(int i=0;i<age.length;i++){
+            System.out.println("Age is : "+age[i]);
+        }
+    }
+    
+}
