6993cb797a21848ea72cc1383dfbe8a17f0064d7
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-30 23:32:54 +0530

    Switch case program

---
+++ SwitchEg.java
@@ -0,0 +1,37 @@
+package ControlStatement;
+
+public class SwitchEg {
+
+	public static void main(String[] args) {
+		
+		int num_1=9;
+		switch(num_1){//switch expression
+		//case statement(multiple condition)
+		case 1:
+			System.out.println("20");
+			break;
+			
+		
+	case 2:
+		System.out.println("15");
+		break;
+		
+	
+	case 9:
+		System.out.println("9");
+		break;
+	
+
+	case 4:
+		System.out.println("11");
+		break;
+	
+	case 5:
+		System.out.println("12");
+		break;
+	default:System.out.println("Invalid number");
+	
+}
+	}
+
+}
