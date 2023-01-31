eacdb77c1f65f2f80749329de42cbddef3f69c56
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-31 23:47:20 +0530

    for each loop to print element of array

---
+++ For_Each.java
@@ -0,0 +1,12 @@
+package ControlStatement;
+
+public class For_Each {
+
+	public static void main(String[] args) {
+		
+		int a[]= {20,30,40,55,22};
+		for(int i:a) {
+			System.out.println(i);
+		}
+
+	}}
