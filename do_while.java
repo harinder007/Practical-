d28fc1274ffba351595609d5f62976a4e8aa3658
Harinder <96901742+harinder007@users.noreply.github.com>
2023-01-31 23:40:00 +0530

    print first 10 no. using do while

---
+++ do_while.java
@@ -0,0 +1,16 @@
+package ControlStatement;
+
+public class do_while {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=1;
+System.out.println("first 10 odd no. using do whilw");
+do {
+
+	System.out.println(i);
+	i=i+2;
+}  while(i<=20);
+	}
+
+}
