83a309116b7a801ccbda6893764da0fa25c8e1bc
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-06 23:21:55 +0530

    find the character position in String

---
+++ Character.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+import java.util.*;
+public class Character {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any word");
+        String st=sc.nextLine();
+        for(int i=1;i<=st.length();i++){
+            System.out.println("Chracter at "+i+" position is :"+st.charAt(i));
+        }
+
+    }
+    
+}
