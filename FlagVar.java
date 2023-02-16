7168c77d9fd98c984afa211adcb7ce8c82aa834b
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-16 23:01:28 +0530

    use of flag variable

---
+++ FlagVar.java
@@ -0,0 +1,18 @@
+public class FlagVar {
+    public static void main(String[] args) {
+        int a=5; int flag=0;
+        int b=9;
+        if(a!=5)
+        flag=0;
+
+     else if(a==5)
+    flag=1;
+    if(flag==1){
+        System.out.println("equal");
+    }
+
+
+
+    }
+    
+}
