67288916c1a317641f297e92f2b41ec98fede3bb
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-18 23:22:38 +0530

    return type statement

---
+++ reutrn.java
@@ -0,0 +1,14 @@
+public class reutrn {
+
+    
+       int sum(int x,int y){
+          int sum=x+y;
+        return sum;
+    }
+    public static void main(String[] args) {
+        int sum;
+         sum=sum(2, 6);
+         System.out.println("Sum is"+sum);
+
+    }
+}
