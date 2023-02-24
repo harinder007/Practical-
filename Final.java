377f20eac116d2211f8d6daa138935cc46360af8
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-24 22:34:14 +0530

    final variable

---
+++ Final.java
@@ -0,0 +1,17 @@
+package OverLoading;
+
+public class Final {
+
+	
+	void run() {
+		final int speed=60;
+		System.out.println("Speed limit of vechile "+speed+"km/h");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Final f=new Final();
+f.run();
+	}
+
+}
