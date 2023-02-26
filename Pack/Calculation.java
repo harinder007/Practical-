ac23187f1bfdabbc1ef24f0adc6fac56be052563
Harinder <96901742+harinder007@users.noreply.github.com>
2023-02-26 22:51:34 +0530

    Package Concept

---
+++ Pack/Calculation.java
@@ -0,0 +1,12 @@
+package Pack;
+public class Calculation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		package1 obj=new package1();
+		obj.sum(4, 5);
+
+	}
+
+}
