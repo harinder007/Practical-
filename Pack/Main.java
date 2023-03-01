56642c72276cb4d7fc930ae416278ad56fb23de4
Harinder <96901742+harinder007@users.noreply.github.com>
2023-03-01 16:38:48 +0530

    Same Package progaram

---
+++ Pack/Main.java
@@ -0,0 +1,9 @@
+package myPackage;
+
+public class Main {
+public static void main(String[] args) {
+	person p=new person("sham",15);
+	
+	
+}
+}
---
+++ Pack/person.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+public class person {
+
+	private String name;
+	private int age;
+
+	person(String name,int age){
+		
+		this.name=name;
+		this.age=age;
+
+		System.out.println("Name is : "+name);
+		System.out.println("Age is : "+age);
+
+	}
+	
+}
