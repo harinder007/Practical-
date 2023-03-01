f92accaed189b8cde7a291d532b985b037b7cdd8
Harinder <96901742+harinder007@users.noreply.github.com>
2023-03-01 16:41:52 +0530

    get method for read only

---
+++ Pack/Encap_read.java
@@ -0,0 +1,19 @@
+package myPackage;
+
+public class Encap_read {
+
+	private String name="Ram";
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Encap_read r=new Encap_read();
+		System.out.println(r.name);
+	}
+
+}
