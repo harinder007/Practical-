c51704c1bbef03c4051b580fc4519ca4404b431f
Harinder <96901742+harinder007@users.noreply.github.com>
2023-03-02 23:36:22 +0530

    Class file of Movie name

---
+++ Pack/Movie.java
@@ -0,0 +1,37 @@
+package myPackage;
+
+public class Movie {
+	private String MovieName;
+	private String MovieStartTime;
+	private String MovieEndTime;
+	private int MovieTicketPrice;
+	//getter setter method is used
+	
+	public String getMovieName() {
+		return MovieName;
+	}
+	public void setMovieName(String movieName) {
+		MovieName = movieName;
+	}
+	public String getMovieStartTime() {
+		return MovieStartTime;
+	}
+	public void setMovieStartTime(String movieStartTime) {
+		MovieStartTime = movieStartTime;
+	}
+	public String getMovieEndTime() {
+		return MovieEndTime;
+	}
+	public void setMovieEndTime(String movieEndTime) {
+		MovieEndTime = movieEndTime;
+	}
+	public int getMovieTicketPrice() {
+		return MovieTicketPrice;
+	}
+	public void setMovieTicketPrice(int movieTicketPrice) {
+		MovieTicketPrice = movieTicketPrice;
+	}
+	
+	
+
+}
