3f1a67c5f8edf2f8c5320cad20af04f30d815880
Harinder <96901742+harinder007@users.noreply.github.com>
2023-03-08 23:48:25 +0530

    Reverse Natural number

--- ReverseNatural.java
+++ ReverseNatural.java
@@ -1,16 +1,21 @@
-package ControlStatement;
-import java.util.*;
+package Assignment;
+
+//Java Program to Print Natural Numbers in Reverse 
+import java.util.Scanner;
+
 public class ReverseNatural {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		System.out.println("Enter any number : ");
-		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
-		for(int i=num;i>=1;i--) {
-			System.out.println(i);
-		}
+	private static Scanner sc;
+	public static void main(String[] args) 
+	{
+		int number, i;
+		sc = new Scanner(System.in);
+		
+		System.out.println(" Please Enter the Maximum integer Value : ");
+		number = sc.nextInt();	
+		
+		for(i = number; i >= 1; i--)
+		{
+			System.out.println(i +"\t"); 
+		}	
 	}
-
 }
