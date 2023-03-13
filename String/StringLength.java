8274a5805ec67eca4ff4b50cb4bd400af99a7186
Harinder <96901742+harinder007@users.noreply.github.com>
2023-03-13 23:39:55 +0530

    String Length

---
+++ String/StringLength.java
@@ -0,0 +1,18 @@
+package String;
+
+//using length() method
+public class StringLength {
+	public static void main(String[] args) {
+		String s="Anudip Foundation";
+		//one way to find the string length
+		System.out.println("The String length of"+s+" is "+s.length());
+		//get the length of string by another way.
+		
+		int len=s.length(); //length of the string
+		
+		System.out.println("The String length of"+s+"is :"+len);
+		
+		//it is more revalent method becoz it saves line of code
+	}
+
+}
