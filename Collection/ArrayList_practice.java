13bcd83e8a6f652ade22c22ed401b91d79437075
Harinder <96901742+harinder007@users.noreply.github.com>
2023-04-09 22:43:35 +0530

    array list practice

---
+++ Collection/ArrayList_practice.java
@@ -0,0 +1,16 @@
+package Collection;
+import java.util.ArrayList;
+public class ArrayList_practice {
+
+//main class
+public static void main(String[] args) {//main method
+ArrayList<String> al=new ArrayList<String>();//creating array list
+//all element in arraylist through object
+al.add("Harinder");
+al.add("Deepak");
+al.add("Amit");
+al.add("Kapil");
+//printing array list
+System.out.println(al);
+}
+}
