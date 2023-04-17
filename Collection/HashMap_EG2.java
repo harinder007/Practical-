7f6ff718edcbd5b421bd5f3a827e65129f53439b
Harinder <96901742+harinder007@users.noreply.github.com>
2023-04-17 23:25:38 +0530

    Hash Map eg 2

---
+++ Collection/HashMap_EG2.java
@@ -0,0 +1,32 @@
+package Collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMap_EG2 {
+public static void main(String[] args) {
+	//creating map
+		HashMap<Integer,String>  map= new HashMap<Integer,String>();
+	map.put(101, "Mango");
+	map.put(108,"Orange");
+	map.put(105,"Banana");//duplicacy will not allow 
+	map.put(103, "Grapes");
+	map.put(105,"Chiku");
+	map.put(null, null);//it will not print
+	map.put(102, null);
+	map.put(null, "Apple");
+	System.out.println(map);
+	//traversing
+	for(Map.Entry m:map.entrySet()) {
+		System.out.println(m.getKey()+" "+m.getValue());//print the integer and string value
+	}
+	System.out.println("---------------");
+map.putIfAbsent(102, "Custard Apple");	
+map.putIfAbsent(101, "Watermelon");	
+for(Map.Entry m:map.entrySet()) {
+	System.out.println(m.getKey()+" "+m.getValue());
+}
+map.remove(103);
+System.out.println(map);
+}
+}
